6671ee27227fb7c8292b129e564e4500d4891e1e
ud1 <ud1@mail.ru>
2010-05-15 16:11:37 +0000

    git-svn-id: https://subversion.assembla.com/svn/dhtpp@28 378b2bfe-bda8-4793-a318-9f6f6feb21af

--- test/parser.lua
+++ test/parser.lua
@@ -1,7 +1,8 @@
 filename = "1273908923.txt"
-rpc_count_time_delta = 10000
+rpc_count_time_delta = 60000
 succeed_find_value_delta = 50
 failed_find_value_delta = 200
+nodes_number = 0
 
 avg_on_time_val = 0
 avg_off_time_val = 0
@@ -31,37 +32,70 @@ function avg_off_time(str)
 	print("min_time", min_time / 60000)
 end
 
+function nodes_number_func(str)
+	nodes_number = tonumber(str)
+	print("nodes_number", nodes_number)
+end
+
 function count_rpc(t, cnt)
 	t = math.floor(t / rpc_count_time_delta)
 	rpc_count[t] = (rpc_count[t] or 0) + sum
 end
 
-prev_rpc_sum = 0
+prev_rpc = {
+	ping_req = 0,
+	store_req = 0,
+	find_node_req = 0,
+	find_value_req = 0,
+	downlist_req = 0,
+	ping_resp = 0,
+	store_resp = 0,
+	find_node_resp = 0,
+	find_value_resp = 0,
+	downlist_resp = 0
+}
+rpc_last_time = 0
+rpc_count = {
+	ping = 0,
+	store = 0,
+	find_node = 0,
+	find_value = 0,
+	downlist = 0
+}
 function rpcs(str)
-	--print(str)
 	_, _, t,
-	ping_reqs, store_req, find_node_req, find_value_req, downlist_req,
+	ping_req, store_req, find_node_req, find_value_req, downlist_req,
 	ping_resp, store_resp, find_node_resp, find_value_resp, downlist_resp = 
 	string.find(str, "(%d+);(%d+);(%d+);(%d+);(%d+);(%d+);(%d+);(%d+);(%d+);(%d+);(%d+)")
 	
 	t = tonumber(t)
-	ping_reqs = tonumber(ping_reqs)
-	store_req = tonumber(store_req)
-	find_node_req = tonumber(find_node_req)
-	find_value_req = tonumber(find_value_req)
-	downlist_req = tonumber(downlist_req)
-	ping_resp = tonumber(ping_resp)
-	store_resp = tonumber(store_resp)
-	find_node_resp = tonumber(find_node_resp)
-	find_value_resp = tonumber(find_value_resp)
-	downlist_resp = tonumber(downlist_resp)
+	prev_rpc.ping_req,			ping_req		= tonumber(ping_req),			tonumber(ping_req)			- prev_rpc.ping_req
+	prev_rpc.store_req,			store_req		= tonumber(store_req),			tonumber(store_req)			- prev_rpc.store_req
+	prev_rpc.find_node_req,		find_node_req	= tonumber(find_node_req),		tonumber(find_node_req)		- prev_rpc.find_node_req
+	prev_rpc.find_value_req,	find_value_req	= tonumber(find_value_req),		tonumber(find_value_req)	- prev_rpc.find_value_req
+	prev_rpc.downlist_req,		downlist_req	= tonumber(downlist_req),		tonumber(downlist_req)		- prev_rpc.downlist_req
+	prev_rpc.ping_resp,			ping_resp		= tonumber(ping_resp),			tonumber(ping_resp)			- prev_rpc.ping_resp
+	prev_rpc.store_resp,		store_resp		= tonumber(store_resp),			tonumber(store_resp)		- prev_rpc.store_resp
+	prev_rpc.find_node_resp,	find_node_resp	= tonumber(find_node_resp),		tonumber(find_node_resp)	- prev_rpc.find_node_resp
+	prev_rpc.find_value_resp,	find_value_resp = tonumber(find_value_resp),	tonumber(find_value_resp)	- prev_rpc.find_value_resp
+	prev_rpc.downlist_resp,		downlist_resp	= tonumber(downlist_resp),		tonumber(downlist_resp)		- prev_rpc.downlist_resp
 	
-	sum = ping_reqs + store_req + find_node_req + find_value_req + downlist_req +
+	sum = ping_req + store_req + find_node_req + find_value_req + downlist_req +
 		ping_resp + store_resp + find_node_resp + find_value_resp + downlist_resp
-	sum, prev_rpc_sum = sum - prev_rpc_sum, sum
 	count_rpc(t, sum)
+	
+	rpc_last_time = t
+	if t < min_time then return end
+	
+	rpc_count.ping			= rpc_count.ping		+ ping_req			+ ping_resp
+	rpc_count.store			= rpc_count.store		+ store_req			+ store_resp
+	rpc_count.find_node		= rpc_count.find_node	+ find_node_req		+ find_node_resp
+	rpc_count.find_value	= rpc_count.find_value	+ find_value_req	+ find_value_resp
+	rpc_count.downlist		= rpc_count.downlist	+ downlist_req		+ downlist_resp
 end;
 
+avg_routing_table_contacts = 0
+avg_routing_table_contacts_count = 0
 function node_info(str)
 	_, _,
 	closest_contactsN, closest_contacts_activeN,
@@ -79,6 +113,11 @@ function node_info(str)
 	pos = math.floor(10*routing_table_activeN / (routing_tableN+1))
 	routing_table_hist[pos] = 
 	(routing_table_hist[pos] or 0) + 1
+	
+	if routing_tableN > 0 then
+		avg_routing_table_contacts = avg_routing_table_contacts + routing_table_activeN / routing_tableN
+	end
+	avg_routing_table_contacts_count = avg_routing_table_contacts_count + 1
 end
 
 
@@ -173,6 +212,7 @@ end
 functions = {}
 functions["avg_on_time"] = avg_on_time
 functions["avg_off_time"] = avg_off_time
+functions["nodes_number"] = nodes_number_func
 functions["rpcs"] = rpcs
 functions["node_info"] = node_info
 functions["succeed_find_value"] = succeed_find_value
@@ -192,6 +232,19 @@ while true do
 end
 
 
+rpc_period = (rpc_last_time - min_time) / 1000
+print("rpc count")
+for i, k in ipairs(rpc_count) do
+	print(i, k)
+end
+print()
+
+print("ping", rpc_count.ping / (rpc_period*nodes_number))
+print("store", rpc_count.store / (rpc_period*nodes_number))
+print("find_node", rpc_count.find_node / (rpc_period*nodes_number))
+print("find_value", rpc_count.find_value / (rpc_period*nodes_number))
+print("downlist", rpc_count.downlist / (rpc_period*nodes_number))
+print()
 
 avg_closest_contacts = 0
 avg_closest_contacts_count = 0
@@ -204,14 +257,16 @@ for i = 0, 10 do
 end
 avg_closest_contacts = avg_closest_contacts / avg_closest_contacts_count
 print("avg_closest_contacts", avg_closest_contacts)
-
+print()
 
 
 print("routing_table_hist")
 for i = 0, 10 do
 	print(i, (routing_table_hist[i] or 0))
 end
-
+avg_routing_table_contacts = avg_routing_table_contacts / avg_routing_table_contacts_count
+print("avg_routing_table_contacts", avg_routing_table_contacts)
+print()
 
 
 print("succeed_find_value_duration_hist")
@@ -220,6 +275,7 @@ for i = 0, succeed_find_value_max_duration do
 end
 avg_succeed_findvalue_duration = avg_succeed_findvalue_duration / avg_succeed_findvalue_count
 print("avg_succeed_findvalue_duration", avg_succeed_findvalue_duration)
+print()
 
 
 print("failed_find_value_duration_hist")
@@ -230,6 +286,8 @@ avg_failed_findvalue_duration = avg_failed_findvalue_duration / avg_failed_findv
 print("avg_failed_findvalue_duration", avg_failed_findvalue_duration)
 
 print("failed/succed", avg_failed_findvalue_count / avg_succeed_findvalue_count)
+print()
+
 
 avg_findnode = 0
 findnode_count = 0
@@ -242,6 +300,7 @@ for i = 0, findnode_max do
 end
 avg_findnode = avg_findnode / findnode_count
 print("avg_findnode", avg_findnode)
+print()
 
 
 avg_findvalue = 0
@@ -255,3 +314,4 @@ for i = 0, findvalue_max do
 end
 avg_findvalue = avg_findvalue / findvalue_count
 print("avg_findvalue", avg_findvalue)
+print()
